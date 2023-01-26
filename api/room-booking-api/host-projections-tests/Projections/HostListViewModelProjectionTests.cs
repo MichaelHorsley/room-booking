477e049200f611baaef33122a54b2cf3f56eb9a5
Michael Horsley <omega_mike@hotmail.com>
2023-01-26 20:06:01 +0000

    Add tests for projection

---
+++ api/room-booking-api/host-projections-tests/Projections/HostListViewModelProjectionTests.cs
@@ -0,0 +1,76 @@
+﻿using events;
+using host_projections.Projections;
+using host_projections.Repositories;
+using Moq;
+using NUnit.Framework;
+using view_models;
+
+namespace host_projections_tests.Projections
+{
+    [TestFixture]
+    public class HostListViewModelProjectionTests
+    {
+        private HostListViewModelProjection _sut;
+
+        private Mock<IViewModelRepository> _mockRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new Mock<IViewModelRepository>();
+            _sut = new HostListViewModelProjection(_mockRepository.Object);
+        }
+
+        [Test]
+        public void SavesObjectWithCorrectId()
+        {
+            var hostRegisteredEvent = new HostRegisteredEvent
+            {
+                AggregateId = Guid.NewGuid().ToString()
+            };
+
+            _sut.Handle(hostRegisteredEvent);
+
+            _mockRepository.Verify(x => x.Save(It.Is<AllHostsViewModel>(y => y.Id.Equals(hostRegisteredEvent.AggregateId))));
+        }
+
+        [Test]
+        public void SavesObjectWithCorrectFirstName()
+        {
+            var hostRegisteredEvent = new HostRegisteredEvent
+            {
+                FirstName = Guid.NewGuid().ToString()
+            };
+
+            _sut.Handle(hostRegisteredEvent);
+
+            _mockRepository.Verify(x => x.Save(It.Is<AllHostsViewModel>(y => y.FirstName.Equals(hostRegisteredEvent.FirstName))));
+        }
+
+        [Test]
+        public void SavesObjectWithCorrectSurname()
+        {
+            var hostRegisteredEvent = new HostRegisteredEvent
+            {
+                Surname = Guid.NewGuid().ToString()
+            };
+
+            _sut.Handle(hostRegisteredEvent);
+
+            _mockRepository.Verify(x => x.Save(It.Is<AllHostsViewModel>(y => y.Surname.Equals(hostRegisteredEvent.Surname))));
+        }
+
+        [Test]
+        public void SavesObjectWithCorrectEmail()
+        {
+            var hostRegisteredEvent = new HostRegisteredEvent
+            {
+                Email = Guid.NewGuid().ToString()
+            };
+
+            _sut.Handle(hostRegisteredEvent);
+
+            _mockRepository.Verify(x => x.Save(It.Is<AllHostsViewModel>(y => y.Email.Equals(hostRegisteredEvent.Email))));
+        }
+    }
+}
